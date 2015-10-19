b0851ca31ad2c4c0bf834779fa9c555224b8153f
Richard Feldman <richard.t.feldman@gmail.com>
2015-10-19 05:57:18 -0700

    Remove redundant merger check

--- src/seamless-immutable.js
+++ src/seamless-immutable.js
@@ -261,7 +261,7 @@
     function addToResult(currentObj, otherObj, key) {
       var immutableValue = Immutable(otherObj[key]);
       var mergerResult = merger && merger(currentObj[key], immutableValue, config);
-      if (merger && mergerResult && mergerResult === currentObj[key]) return;
+      if (mergerResult && mergerResult === currentObj[key]) return;
 
       anyChanges = anyChanges ||
         mergerResult !== undefined ||
