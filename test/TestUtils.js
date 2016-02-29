bc1851c4eb96a23aa03120deff1a6136c6d602dc
Richard Feldman <richard.t.feldman@gmail.com>
2016-02-29 11:17:29 -0800

    Update package.json dependencies.

--- test/TestUtils.js
+++ test/TestUtils.js
@@ -40,7 +40,7 @@ function assertHasPrototype(obj, expectedPrototype) {
 }
 
 function withoutItengerKeys(obj) {
-  return _.object(_.map(obj, function(value, key) {
+  return _.fromPairs(_.map(obj, function(value, key) {
     // Don't choose keys that can be parsed as 32-bit unsigned integers,
     // as browsers make no guarantee on key ordering for those,
     // and we rely on ordered keys to simplify several tests.
@@ -55,7 +55,7 @@ function withoutItengerKeys(obj) {
 // Returns an object which may or may not contain nested objects and arrays.
 function ComplexObjectSpecifier() {
   return function() {
-    var obj = _.object(_.map(JSC.array()(), function() {
+    var obj = _.fromPairs(_.map(JSC.array()(), function() {
       var key   = JSC.string()();
       var value = JSC.one_of([JSC.array(), JSC.object(),
         JSC.falsy(), JSC.integer(), JSC.number(), JSC.string(),
