1e9d0e3067b9048264b3cce090bab14849d77d49
zepod <albert.nemec2457@gmail.com>
2017-05-16 15:42:21 +0200

    add reduce method for immutable objects

--- test/ImmutableObject.spec.js
+++ test/ImmutableObject.spec.js
@@ -1,11 +1,12 @@
 var testMerge     = require("./ImmutableObject/test-merge.js");
-var testReplace     = require("./ImmutableObject/test-replace.js");
+var testReplace   = require("./ImmutableObject/test-replace.js");
 var testCompat    = require("./ImmutableObject/test-compat.js");
 var testWithout   = require("./ImmutableObject/test-without.js");
 var testAsMutable = require("./ImmutableObject/test-asMutable.js");
 var testSet       = require("./ImmutableObject/test-set.js");
 var testUpdate    = require("./ImmutableObject/test-update.js");
-var testGetIn    = require("./ImmutableObject/test-getIn.js");
+var testGetIn     = require("./ImmutableObject/test-getIn.js");
+var testReduce    = require("./ImmutableObject/test-reduce");
 var devBuild      = require("../seamless-immutable.development.js");
 var prodBuild     = require("../seamless-immutable.production.min.js");
 var getTestUtils  = require("./TestUtils.js");
@@ -27,6 +28,7 @@ var getTestUtils  = require("./TestUtils.js");
       testSet(config);
       testUpdate(config);
       testGetIn(config);
+      testReduce(config)
     });
   });
 });
